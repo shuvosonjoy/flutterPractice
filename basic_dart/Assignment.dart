8798cdd8a5ce040c958e39213ff8929cbd2db420
shuvosonjoy <105931179+shuvosonjoy@users.noreply.github.com>
2023-08-14 00:28:52 +0600

    Initial commit

---
+++ basic_dart/Assignment.dart
@@ -0,0 +1,14 @@
+
+
+void main() {
+  List<double>priceList=[12.99, 8.75, 21.50, 5.00];
+  double sum=0,tax=0.08;
+  for(double i in priceList){
+    sum+=i;
+  }
+
+  double ans=((sum*tax)+sum);
+  String withTaxPrice = ans.toStringAsFixed(2);
+
+  print('Total Cost: \$${withTaxPrice}');
+}
