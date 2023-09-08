67c1154387eb596668e66e254fcdf0f0344e6557
shuvosonjoy <105931179+shuvosonjoy@users.noreply.github.com>
2023-09-08 16:05:43 +0600

    commit to new branch

--- lib/Assignment_5.dart
+++ lib/Assignment_5.dart
@@ -1,60 +1,86 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(
-      myApp()
-  );
+  runApp(myApp());
 }
 
 
 class myApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: HomeScreen(),
-      title: 'Hello app',
+      title: "Module_5",
     );
   }
 }
 
 
+class HomeScreen extends StatelessWidget {
 
-class HomeScreen extends StatelessWidget{
-
-  @override
-  Widget build(BuildContext context){
-
-    return  Scaffold(
-
-      backgroundColor: Colors.blueGrey,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        toolbarHeight: 100,
+        elevation: 70,
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        leading: Icon(Icons.add_card_sharp),
+        actions: [
+          Row(
+            children: [
 
-        backgroundColor: Colors.deepOrangeAccent,
-        //Or CenterTitle: true,
-        //title: Text("home")
-        title:
-        Center(
-          child:Text("Home"),
+                Icon(Icons.search),
 
-        ),
-        leading: Icon(Icons.home,color: Colors.tealAccent,),
 
-      ),
-      body:
 
-      Column(
-        children: [
-          Text("hello world",style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-            color: Colors.amber,
-          ),),Text("Homepages"),
-          Icon(Icons.access_time_filled),
+            ],
+          ),
         ],
 
 
+        title: Text(
+          "Home",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "This is mod 5 Assignment",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "My ",
+                  style: TextStyle(color: Colors.red, fontSize: 25),
+                ),
+                Text(
+                  "phone ",
+                  style: TextStyle(color: Colors.blue, fontSize: 15),
+                ),
+                Text(
+                  "name ",
+                  style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Your phone name ",
+                  style: TextStyle(color: Colors.amber, fontSize: 25),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
