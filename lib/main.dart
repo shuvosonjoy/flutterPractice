707167653e0356515d3370d49d92208e5a3a800d
shuvosonjoy <shuvosdg123@gmail.com>
2023-09-20 02:55:55 +0600

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -13,35 +13,89 @@ class myApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<String> students = [
-    'shuvo',
-    'sonjoy',
-    'shuvo_sonjoy',
-    'Sonjoy Sutradhar Shuvo'
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Scrollbar(
-        child: ListView.separated(
-          itemCount: students.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(students[index]),
-            );
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SettingScreen()));
+                },
+                child: Text("SettingsScreen")),
+            IconButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => OrderScreen()));
+                },
+                icon: Icon(Icons.account_circle))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("This is Settings screen"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Order"),
+        leading: GestureDetector(
+          child: Icon(Icons.account_balance_rounded),
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomeScreen()));
           },
-          separatorBuilder: (context, index) {
-            return Divider();
-          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("This is order Screen"),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(   ///kill previous page
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()),
+                      (route) => false);
+                },
+                child: Text("Home"))
+          ],
         ),
       ),
     );
