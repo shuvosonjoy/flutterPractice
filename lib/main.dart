d754d9e85a333fe7a2f174950fef8812d7667c15
shuvosonjoy <shuvosdg123@gmail.com>
2023-09-19 18:40:32 +0600

    Listview_separated

--- lib/main.dart
+++ lib/main.dart
@@ -23,29 +23,22 @@ class HomeScreen extends StatelessWidget {
         title: Text("Appbar"),
       ),
       body: Scrollbar(
-        /*child: GridView.builder(
-          itemCount: 10, ///how much item needed
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemBuilder: (context, index) {
-            return Column(
-              children: [
-                
-                Icon(Icons.account_circle_sharp),
-                Text("item no ${index+1}"),
-              ],
-            );
-          },
-        ),
-        */
-        child: ListView.builder(
+
+        child: ListView.separated(
             itemCount: 20,
             itemBuilder: (context, index) {
               return ListTile(
                 title: Text("list tile index: ${index + 1}"),
                 subtitle: Text("subtitle....."),
                 leading: Icon(Icons.account_balance),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return Column(
+                children: [
+                  Text(index.toString()),
+                  Divider(),
+                ],
               );
             }),
       ),
