89276ea7f76253b3a6e1305ae86a0e9b7b6f9748
shuvosonjoy <shuvosdg123@gmail.com>
2023-09-19 17:53:42 +0600

    Gridview_builder_basic

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Appbar"),
+      ),
+      body: Scrollbar(
+        child: GridView.builder(
+          itemCount: 10, ///how much item needed
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                
+                Icon(Icons.account_circle_sharp),
+                Text("item no ${index+1}"),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
