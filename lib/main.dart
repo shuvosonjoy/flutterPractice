af3d395dfef820847ce975c4089177c024d323ad
shuvosonjoy <shuvosdg123@gmail.com>
2023-09-24 23:44:40 +0600

    mod7_live

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
@@ -23,58 +22,77 @@ class CounterScreen extends StatefulWidget {
   State<StatefulWidget> createState() => _CounterState();
 }
 
-/// state
 class _CounterState extends State<CounterScreen> {
-
   int counter = 0;
+
+  void _showAlertDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Button Pressed $counter times'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Counter App'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text(" Count : ",style: TextStyle(fontSize: 25),),
             Text(
               counter.toString(),
               style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                ElevatedButton(onPressed: () {
-                  counter = counter + 1;
-                  setState(() {});
-
-                },
-
-                    child: Text('+')),
-               SizedBox(width: 10,),
-
-                ElevatedButton(onPressed: () {
-                  counter = counter - 1;
-                  setState(() {});
-
-                },
-                    child: Text('-')),
-
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter = counter + 1;
+                      if (counter >= 5) {
+                        _showAlertDialog();
+                      }
+                    });
+                  },
+                  child: Text("+",style: TextStyle(fontSize: 20),)
+                ),
+                SizedBox(width: 15),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (counter > 0) {
+                        counter = counter - 1;
+                      }
+                    });
+                  },
+                  child: Text("-",style: TextStyle(fontSize: 20),),
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
-
-
     );
   }
 }
-
-
-
