89d2987d5f78534dd8cfc39f28355151eea4b715
shuvosonjoy <shuvosdg123@gmail.com>
2023-09-25 04:08:37 +0600

    statefull_counter

--- lib/main.dart
+++ lib/main.dart
@@ -5,175 +5,78 @@ void main() {
 }
 
 class myApp extends StatelessWidget {
+  const myApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SettingScreen()));
-                },
-                child: Text("SettingsScreen")),
-            IconButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderScreen()));
-                },
-                icon: Icon(Icons.account_circle)),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return productScreen();
-                      },
-                    ),
-                  );
-                },
-                child: Text("Product_Screen")),
+            Text(counter.toString()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        counter++;
+
+                        if (counter >= 5) _dialogueBox();
+                      });
+                    },
+                    child: Icon(Icons.add)),
+                ElevatedButton(
+                    onPressed: () {
+                      if (counter > 0) counter--;
+                      setState(() {});
+                    },
+                    child: Icon(Icons.minimize)),
+              ],
+            )
           ],
         ),
       ),
     );
   }
-}
 
-class SettingScreen extends StatelessWidget {
-  const SettingScreen({super.key});
+  void _dialogueBox() {
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Settings"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("This is Settings screen"),
-          ],
-        ),
-      ),
-    );
-  }
-}
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
 
-class OrderScreen extends StatelessWidget {
-  const OrderScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Order"),
-        leading: GestureDetector(
-          child: Icon(Icons.account_balance_rounded),
-          onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => HomeScreen()));
-          },
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("This is order Screen"),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushAndRemoveUntil(
-
-                      ///kill previous page
-                      context,
-                      MaterialPageRoute(builder: (context) => HomeScreen()),
-                      (route) => false);
-                },
-                child: Text("Home"))
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class productScreen extends StatelessWidget {
-  const productScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Product Screen"),
-      ),
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return ListTile(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => productDetails(
-                        productName: 'product ${index.toString()}',
-                        price: 1234,
-                      ),
-                    )).then((value) {
-                  print(value);
-                });
+        return AlertDialog(
+          title: Text('Button Pressed $counter times'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
               },
-              title: Text(index.toString()),
-            );
-          }),
-    );
-  }
-}
-
-class productDetails extends StatelessWidget {
-  final String productName;
-  final double? price;
-
-  const productDetails({super.key, required this.productName, this.price});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Details"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(productName),
-            Text(price.toString()),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context, 'pop_name$productName');
-                },
-                child: Text("Back"))
+            ),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
 }
