6cd08a9d1f82f9b17fd57c77c34e0e0e027f9f28
shuvosonjoy <shuvosdg123@gmail.com>
2023-09-25 21:40:39 +0600

    mod7_assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,141 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'Mod7_Assignment',
+      home: ProductList(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
+class ProductList extends StatefulWidget {
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _ProductListState createState() => _ProductListState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  int counter = 0;
+class _ProductListState extends State<ProductList> {
+  List<Product> products = [
+    Product('Product 1', 10.0),
+    Product('Product 2', 20.0),
+    Product('Product 3', 30.0),
+    Product('Product 4', 10.0),
+    Product('Product 5', 50.0),
+  ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(counter.toString()),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        counter++;
+  List<String> cartCategories = [];
 
-                        if (counter >= 5) _dialogueBox();
-                      });
-                    },
-                    child: Icon(Icons.add)),
-                ElevatedButton(
-                    onPressed: () {
-                      if (counter > 0) counter--;
-                      setState(() {});
-                    },
-                    child: Icon(Icons.minimize)),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+  void _addToCart(int index) {
+    setState(() {
+      products[index].CartQuantity++;
+      if (!cartCategories.contains(products[index].ProductName)) {
+        cartCategories.add(products[index].ProductName);
+      }
+      if (products[index].CartQuantity == 5) {
+        _CongratulationsDialog(products[index]);
+      }
+    });
   }
 
-  void _dialogueBox() {
-
+  void _CongratulationsDialog(Product product) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-
         return AlertDialog(
-          title: Text('Button Pressed $counter times'),
-          actions: <Widget>[
+          title: Text(
+            'Congratulations!',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          content: Text('You\'ve bought 5 ${product.ProductName}!'),
+          actions: [
             TextButton(
-              child: Text('Close'),
               onPressed: () {
                 Navigator.of(context).pop();
               },
+              child: Text('OK'),
             ),
           ],
         );
       },
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CartPage(cartCategories: cartCategories),
+            ),
+          );
+        },
+        child: Icon(Icons.shopping_cart),
+      ),
+      appBar: AppBar(
+        title: Center(child: Text('Product List')),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].ProductName),
+            subtitle: Text(
+                'Price: \$${products[index].ProductPrice.toStringAsFixed(2)}'),
+            trailing: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Counters: ${products[index].CartQuantity}'),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.only(
+                          left: 15, right: 15, top: 5, bottom: 5),
+                    ),
+                    onPressed: () {
+                      _addToCart(index);
+                    },
+                    child: Text("Buy Now"))
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CartPage extends StatelessWidget {
+  final List<String> cartCategories;
+
+  CartPage({required this.cartCategories});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Cart')),
+      ),
+      body: Center(
+        child: Text('Total Products: ${cartCategories.length}'),
+      ),
+    );
+  }
+}
+
+class Product {
+  final String ProductName;
+  final double ProductPrice;
+  int CartQuantity = 0;
+
+  Product(this.ProductName, this.ProductPrice);
 }
