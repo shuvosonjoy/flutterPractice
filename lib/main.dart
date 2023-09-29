255733f408684ca55695c93d2aeb5d8eba568634
shuvosonjoy <shuvosdg123@gmail.com>
2023-09-29 23:36:23 +0600

    mod8_live_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,75 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Image Feed',
       home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Apppbar'),
-        centerTitle: true,
+        title: Text('Image Feed'),
       ),
-
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.portrait) {
+            return portraitList();
+          } else {
+            return landscapeGrid();
+          }
+        },
+      ),
     );
   }
 }
+
+class portraitList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final itemCount = 10;
+
+    return ListView.builder(
+      itemCount: itemCount,
+      itemBuilder: (context, index) {
+        return Container(
+          padding: EdgeInsets.all(30),
+          child: Image.network('https://images.pexels.com/photos/19671/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600',width: 150,height: 150,),
+        );
+      },
+    );
+  }
+}
+
+class landscapeGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final itemCount = 10;
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: itemCount,
+      itemBuilder: (context, index) {
+        return Container(
+
+          padding: EdgeInsets.only(top: 8,bottom: 8),
+          child: Image.network('https://images.pexels.com/photos/1787234/pexels-photo-1787234.jpeg?auto=compress&cs=tinysrgb&w=600',width: 150,height: 150,),
+        );
+      },
+    );
+  }
+}
+
+
