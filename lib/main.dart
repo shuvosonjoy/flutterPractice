444a024458da835ebe448174949d9b1c6da1cf9e
shuvosonjoy <shuvosdg123@gmail.com>
2023-09-29 19:18:34 +0600

    mod8_live_test

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Apppbar'),
+        centerTitle: true,
+      ),
+
+    );
+  }
+}
