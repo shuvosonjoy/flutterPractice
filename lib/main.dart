676c55175f8bb45dda0233e3e5de4f2d9a5669f7
shuvosonjoy <shuvosdg123@gmail.com>
2023-09-29 19:00:01 +0600

    mod8_assignmetn

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() {
   runApp(myApp());
 }
 
+
+
 class myApp extends StatelessWidget {
   const myApp({super.key});
 
@@ -29,204 +31,33 @@ class HomePage extends StatelessWidget {
         scrollDirection: Axis.vertical,
         child: LayoutBuilder(builder: (context, constraints) {
           if (constraints.maxWidth < 500) {
+
             return Container(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(
-                   // margin: EdgeInsets.only(left: 10, top: 10),
-
-                    height: 300,
-                    width: 300,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(1000),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            'https://images.pexels.com/photos/1203803/pexels-photo-1203803.jpeg?auto=compress&cs=tinysrgb&w=600',
-                          ),
-                          fit: BoxFit.cover,
-                        )),
-                    //  child: Image.network('https://images.pexels.com/photos/1203803/pexels-photo-1203803.jpeg?auto=compress&cs=tinysrgb&w=600'),
-                  ),
-                  Container(
-                    child: Column(
-                      children: [
-                        Text('John Doe'),
-                        Wrap(
-                          children: [
-                            Text(
-                                'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum'),
-
-                          ],
-                        ),
-                        SizedBox(
-                          height: 300,
-                          child: Container(
-                            child: GridView(
-                                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 3,
-                                  childAspectRatio: 1.2,
-                                ),
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.all(5),
-                                  height: 100,
-                                  width: 100,
-                                  child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.all(5),
-                                  height: 100,
-                                  width: 100,
-                                  child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.all(5),
-                                  height: 100,
-                                  width: 100,
-                                  child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.all(5),
-                                  height: 100,
-                                  width: 100,
-                                  child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.all(5),
-                                  height: 100,
-                                  width: 100,
-                                  child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.all(5),
-                                  height: 100,
-                                  width: 100,
-                                  child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.all(5),
-                                  height: 100,
-                                  width: 100,
-                                  child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.all(5),
-                                  height: 100,
-                                  width: 100,
-                                  child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.all(5),
-                                  height: 100,
-                                  width: 100,
-                                  child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                ),
-
-                              ],
-
-                            ),
-                          ),
-                        ),
-
-                      ],
-                    ),
-                  ),
+                  _centerImage(),
+                  _contentPage(),
                 ],
               ),
             );
-          } else {
+          }
+          else {
             return Container(
-              child:  Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    margin: EdgeInsets.only(left: 10, ),
-
-                    height: 250,
-                    width: 250,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(1000),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            'https://images.pexels.com/photos/1203803/pexels-photo-1203803.jpeg?auto=compress&cs=tinysrgb&w=600',
-                          ),
-                          fit: BoxFit.cover,
-                        )),
-                    //  child: Image.network('https://images.pexels.com/photos/1203803/pexels-photo-1203803.jpeg?auto=compress&cs=tinysrgb&w=600'),
-                  ),
-                  SizedBox(
-                    width: 550,
-                    child: Container(
-                      child: Column(
-                        children: [
-                          Text('John Doe'),
-                          Wrap(
-                            children: [
-                              Text(
-                                  'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum'),
-
-                            ],
-                          ),
-                          SizedBox(
-
-                            height: 300,
-                            width: 900,
-                            child: Container(
-                              child: GridView(
-
-                                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 3,
-                                  childAspectRatio: 1.2,
-                                ),
-                                children: [
-                                  Container(
-                                    margin: EdgeInsets.all(5),
-                                    height: 250,
-                                    width: 250,
-                                    child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.all(5),
-                                    height: 250,
-                                    width: 250,
-                                    child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.all(5),
-                                    height: 250,
-                                    width: 250,
-                                    child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.all(5),
-                                    height: 250,
-                                    width: 250,
-                                    child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.all(5),
-                                    height: 250,
-                                    width: 250,
-                                    child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.all(5),
-                                    height: 250,
-                                    width: 250,
-                                    child:  Image.network('https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
-                                  ),
-
-                                ],
-
-                              ),
-                            ),
-                          ),
-
-                        ],
-                      ),
-                    ),
-                  ),
+                      height: 300,
+                      width: 300,
+                      margin: EdgeInsets.all(20),
+                      child: _centerImage()),
+                  Expanded(
+                      child: Container(
+                          margin: EdgeInsets.only(top: 10, right: 10, left: 0),
+                          child: _contentPage())),
                 ],
               ),
             );
@@ -235,4 +66,72 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+Container _centerImage() {
+  return Container(
+    // margin: EdgeInsets.only(left: 10, top: 10),
+
+    height: 350,
+    width: 350,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(1000),
+        image: DecorationImage(
+          image: NetworkImage(
+            'https://images.pexels.com/photos/1203803/pexels-photo-1203803.jpeg?auto=compress&cs=tinysrgb&w=600',
+          ),
+          fit: BoxFit.cover,
+        )),
+  );
+}
+
+Container _contentPage() {
+  return Container(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'John Doe',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Wrap(
+          children: [
+            Text(
+              'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum lorem ipsum',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          height: 600,
+          width: 900,
+          child: Container(
+            child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.5,
+                ),
+                itemCount: 9,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.all(5),
+                    // height: 500,
+                    // width: 500,
+                    child: Image.network(
+                        'https://images.pexels.com/photos/1205022/pexels-photo-1205022.jpeg?auto=compress&cs=tinysrgb&w=400'),
+                  );
+                }),
+          ),
+        ),
+      ],
+    ),
+  );
 }
